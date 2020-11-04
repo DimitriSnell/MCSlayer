55872b43f7b51f9b932c3ad9a181c778d4d0155f
Dimitri Snell <meach1234567@gmail.com>
2020-11-03 19:25:40 -0500

    properly implemented base gui for SlayerJournal

--- src/main/java/com/meach/mcslayer/client/gui/SlayerJournalGui.java
+++ src/main/java/com/meach/mcslayer/client/gui/SlayerJournalGui.java
@@ -2,6 +2,8 @@ package com.meach.mcslayer.client.gui;
 
 import com.meach.mcslayer.mcslayer;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -15,15 +17,21 @@ public class SlayerJournalGui extends Screen{
 
     public SlayerJournalGui(ITextComponent titleIn){
         super(titleIn);
-        this.field_230708_k_ = 20;
-        this.field_230709_l_ = 500;
     }
 
     @Override
     public void func_230430_a_(MatrixStack p_230430_1_, int p_230430_2_, int p_230430_3_, float p_230430_4_) {
+
+        int x = (this.field_230708_k_-271)/2;
+        int y = (this.field_230709_l_-180)/2;
+
         this.func_230446_a_(p_230430_1_);
-        super.func_230430_a_(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
-        this.getMinecraft().getTextureManager().bindTexture(BACKGROUND_TEXTURE);
-        this.func_238465_a_(p_230430_1_,20,20,20,20);
+        //super.func_230430_a_(p_230430_1_, p_230430_2_, p_230430_3_, p_230430_4_);
+        Minecraft.getInstance().getTextureManager().bindTexture(BACKGROUND_TEXTURE);
+
+        RenderSystem.color4f(1,1,1,1);
+        func_238463_a_(p_230430_1_,x,y,0F,0F,271,180,271,180);
     }
+
+
 }
